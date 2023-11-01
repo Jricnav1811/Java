e09bb3f71a478efbb2addf480aaf94d77c89a020
Jricnav1811 <jricnav1811@g.educaand.es>
2023-11-01 19:47:08 +0100

    dd

---
+++ UT4/Ejercicios/Ejercicio4_7.java
@@ -0,0 +1,9 @@
+package UT4.Ejercicios;
+import java.util.Scanner;
+public class Ejercicio4_7 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    sc.close();
+  }
+}
